5cfc84e84e03918ffd54e7ff4152347da98821e1
rengifolu <andrelujan7@gmail.com>
2022-01-08 14:08:55 +0100

    set up enviroment test

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
+import { environment } from '../../environments/environment';
 
-const baseUrl = 'http://localhost:8080/api/tutorials';
+const baseUrl = environment.baseUrl + '/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
